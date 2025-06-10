dece1e735142daefe77e0bf56acf1bde844fbf91
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-06-10 05:39:24 -0400

    Remove tanstack devtools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { TaskProvider } from '@context/task-context';
 import { getQueryClient } from '@lib/get-query-client';
@@ -11,7 +10,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <TaskProvider>{children}</TaskProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
